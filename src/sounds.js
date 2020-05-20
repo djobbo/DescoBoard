4c7e61bbaf285c453560d3452f4f81533a1c862b
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-05-20 10:59:42 +0200

    ballec

--- src/sounds.js
+++ src/sounds.js
@@ -2,22 +2,25 @@ export const sounds = [
 	{
 		title: 'OUIIIIIIII',
 		thumb: 'src/imgs/Calf.png',
-		description: "Alexis c'est un veau",
 		sound: 'src/sounds/OUIIIIIIIIIIIIIIIII.mp3',
 		author: 'ALFy',
 	},
 	{
 		title: 'SALUT!!! CA VA?!!',
 		thumb: 'src/imgs/Calf.png',
-		description: "Alexis c'est un veau",
 		sound: 'src/sounds/SLTCAVAAAAAAAAA.mp3',
 		author: 'ALFy',
 	},
 	{
 		title: 'JSUIS PAS 1 PD',
 		thumb: 'src/imgs/Calf.png',
-		description: "Alexis c'est un veau",
 		sound: 'src/sounds/CHUISPA1PD-1.mp3',
+		author: 'ALFy',
+	},
+	{
+		title: 'BALLEC BALLEC',
+		thumb: 'src/imgs/Calf.png',
+		sound: 'src/sounds/BALLECBALLEC.mp3',
 		author: 'ALFy',
 	},
 ];
